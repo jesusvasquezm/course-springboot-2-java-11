9ad8058df7339b0ce2468983a4888e1f16b3b88a
Jesus Vasquez M <jesusvasquezm098@gmail.com>
2022-08-02 12:15:06 -0300

    OrderStatus enum

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Order;
 import com.example.course.entities.User;
+import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		//definir as data no padrão ISO 8601
-		Order o1 = new Order(null, Instant.parse("2022-08-02T11:22:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2022-07-02T03:42:10Z"),u2);
-		Order o3 = new Order(null, Instant.parse("2022-07-01T15:21:22Z"),u1);
+		Order o1 = new Order(null, Instant.parse("2022-08-02T11:22:07Z"),OrderStatus.PAID ,u1);
+		Order o2 = new Order(null, Instant.parse("2022-07-02T03:42:10Z"),OrderStatus.WAITING_PAYMENT,u2);
+		Order o3 = new Order(null, Instant.parse("2022-07-01T15:21:22Z"),OrderStatus.WAITING_PAYMENT,u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
