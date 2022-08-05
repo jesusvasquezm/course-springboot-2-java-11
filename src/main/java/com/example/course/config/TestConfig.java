b2fc074fe6abe67b94eb23f872965f3731f86185
Jesus Vasquez M <jesusvasquezm098@gmail.com>
2022-08-05 10:35:40 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
+import com.example.course.entities.OrderItem;
 import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.CategoryRepository;
+import com.example.course.repositories.OrderItemRepository;
 import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.ProductRepository;
 import com.example.course.repositories.UserRepository;
@@ -33,21 +35,26 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired 
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		//Instancias os objetos em memória
 		Category cat1 = new Category(null, "Eletronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
-		
+		//Instancias os objetos em memória
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
+		//Salvar no banco de dados
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
@@ -61,10 +68,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
-
+		//Instancias os objetos em memória
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
+		//Instancias os objetos em memória
 		//definir as data no padrão ISO 8601
 		Order o1 = new Order(null, Instant.parse("2022-08-02T11:22:07Z"),OrderStatus.PAID ,u1);
 		Order o2 = new Order(null, Instant.parse("2022-07-02T03:42:10Z"),OrderStatus.WAITING_PAYMENT,u2);
@@ -72,6 +80,14 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		//Instancias os objetos em memória
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 	}
 }
